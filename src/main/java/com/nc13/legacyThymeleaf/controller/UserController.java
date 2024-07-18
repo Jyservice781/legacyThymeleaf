b3c82b6c5511dfb6b94c38421132171d1687408f
BITCAMP <gimjuyeong512@gmail.com>
2024-07-18 11:36:21 +0900

    commit 02

--- src/main/java/com/nc13/legacyThymeleaf/controller/UserController.java
+++ src/main/java/com/nc13/legacyThymeleaf/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.nc13.legacyThymeleaf.controller;
 
+import com.nc13.legacyThymeleaf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+    @Autowired
+    public UserService userService;
+
 
 }
